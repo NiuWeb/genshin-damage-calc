7b713f74fa4aa8b2a4ef39bc90ddeeb08abf7cbf
Bygdle <niuweb1channel@gmail.com>
2021-04-17 11:39:03 -0500

    dir renamed

--- app.js
+++ app.js
@@ -2,192 +2,6 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./built/characters/characterList.js":
-/*!*******************************************!*\
-  !*** ./built/characters/characterList.js ***!
-  \*******************************************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-
-Object.defineProperty(exports, "__esModule", ({
-  value: true
-}));
-exports.characterList = void 0;
-
-var charHuTao_1 = __webpack_require__(/*! ./list/charHuTao */ "./built/characters/list/charHuTao.js");
-
-exports.characterList = {
-  "Hu Tao": charHuTao_1.charHuTao
-};
-
-/***/ }),
-
-/***/ "./built/characters/list/charHuTao.js":
-/*!********************************************!*\
-  !*** ./built/characters/list/charHuTao.js ***!
-  \********************************************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-
-Object.defineProperty(exports, "__esModule", ({
-  value: true
-}));
-exports.charHuTao = void 0;
-
-var lang_1 = __webpack_require__(/*! ../../langs/lang */ "./built/langs/lang.js");
-
-exports.charHuTao = {
-  name: "Hu Tao",
-  ATKbase: [8, 21, 29, 43, 48, 55, 62, 69, 74, 81, 86, 94, 99, 106],
-  HPbase: [1211, 3141, 4179, 6253, 6990, 8042, 9026, 10089, 10826, 11899, 12637, 13721, 14459, 15552],
-  DEFbase: [68, 177, 235, 352, 394, 453, 508, 568, 610, 670, 712, 773, 815, 876],
-  scaleValue: [0, 0, 0, 0, 0.096, 0.096, 0.192, 0.192, 0.192, 0.192, 0.288, 0.288, 0.384, 0.384],
-  scaleStat: "CRITDMG",
-  NormalAttacks: [{
-    name: "1-Hit",
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "NormalAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.4689, 0.5008, 0.5328, 0.5754, 0.6074, 0.6447, 0.6926, 0.7406, 0.7885, 0.8365, 0.8844, 0.9324, 0.9804, 1.0283, 1.0763]]
-  }, {
-    name: "2-Hit",
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "NormalAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.4825, 0.5154, 0.5483, 0.5922, 0.6251, 0.6635, 0.7128, 0.7622, 0.8115, 0.8609, 0.9102, 0.9596, 1.0089, 1.0583, 1.1076]]
-  }, {
-    name: "3-Hit",
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "NormalAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.6105, 0.6521, 0.6938, 0.7493, 0.7909, 0.8394, 0.9019, 0.9643, 1.0268, 1.0892, 1.1516, 1.2141, 1.2765, 1.3389, 1.4014]]
-  }, {
-    name: "4-Hit",
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "NormalAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.6564, 0.7012, 0.7459, 0.8056, 0.8503, 0.9026, 0.9697, 1.0368, 1.1040, 1.1711, 1.2382, 1.3054, 1.3725, 1.4396, 1.5068]]
-  }, {
-    name: "5-Hit",
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "NormalAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.6847, 0.7314, 0.7781, 0.8404, 0.8870, 0.9415, 1.0115, 1.0816, 1.1516, 1.2216, 1.2917, 1.3617, 1.4317, 1.5018, 1.5718]]
-  }, {
-    name: "6-Hit",
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "NormalAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.8596, 0.9182, 0.9768, 1.0549, 1.1136, 1.1819, 1.2698, 1.3578, 1.4457, 1.5336, 1.6215, 1.7094, 1.7973, 1.8852, 1.9731]]
-  }, {
-    name: lang_1.Language.val("ChargedAttack"),
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "ChargedAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[1.3596, 1.4523, 1.5450, 1.6686, 1.7613, 1.8695, 2.0085, 2.1476, 2.2866, 2.4257, 2.5647, 2.7038, 2.8428, 2.9819, 3.1209]]
-  }, {
-    name: lang_1.Language.val("Plunge"),
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "PlungeAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.6542, 0.6988, 0.7434, 0.8029, 0.8475, 0.8995, 0.9664, 1.0333, 1.1002, 1.1671, 1.2340, 1.3010, 1.3679, 1.4348, 1.5017]]
-  }, {
-    name: lang_1.Language.val("LowPlunge"),
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "PlungeAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[1.3081, 1.3973, 1.4865, 1.6054, 1.6946, 1.7986, 1.9324, 2.0662, 2.2000, 2.3338, 2.4676, 2.6013, 2.7351, 2.8689, 3.0027]]
-  }, {
-    name: lang_1.Language.val("HighPlunge"),
-    elementalDMG: "PhysicalDMG",
-    talentDMG: "PlungeAttackDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[1.6339, 1.7453, 1.8567, 2.0052, 2.1166, 2.2466, 2.4137, 2.5808, 2.7479, 2.9150, 3.0821, 3.2492, 3.4163, 3.5834, 3.7505]]
-  }],
-  ElementalSkills: [{
-    name: lang_1.Language.val("BloodBlossom"),
-    elementalDMG: "PyroDMG",
-    talentDMG: "ElementalSkillDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[0.6400, 0.6880, 0.7360, 0.8000, 0.8480, 0.8960, 0.9600, 1.0240, 1.0880, 1.1520, 1.2160, 1.2800, 1.3600, 1.4400, 1.5200]]
-  }, {
-    name: lang_1.Language.val("ParamitaPapilio"),
-    description: lang_1.Language.val("ParamitaPapilioDesc"),
-    effect: function (e) {
-      var bonuses = [0.0384, 0.0407, 0.0430, 0.0460, 0.0483, 0.0506, 0.0536, 0.0566, 0.0596, 0.0626, 0.0656, 0.0685, 0.0715, 0.0745, 0.0775];
-      var bonus = bonuses[e.ElementalSkillLevel - 1];
-      var ATKincrease = e.createModifier("ATKflat");
-      ATKincrease.value = Math.min(bonus * e.HP, 4 * e.ATKbase);
-      e.createObserver("HPbase", "HPflat", "HPpercent", "ATKbase", "ElementalSkillLevel").onUpdate(function (e) {
-        bonus = bonuses[e.ElementalSkillLevel - 1];
-        ATKincrease.value = Math.min(bonus * e.HP, 4 * e.ATKbase);
-        ;
-      });
-      ATKincrease.onEnable(function (e) {
-        e.infuseDamageInstance("NormalAttackDMG", "PyroDMG");
-        e.infuseDamageInstance("ChargedAttackDMG", "PyroDMG");
-        e.infuseDamageInstance("PlungeAttackDMG", "PyroDMG");
-      });
-      ATKincrease.onDisable(function (e) {
-        e.infuseDamageInstance("NormalAttackDMG", null);
-        e.infuseDamageInstance("ChargedAttackDMG", null);
-        e.infuseDamageInstance("PlungeAttackDMG", null);
-      });
-      return [ATKincrease];
-    }
-  }],
-  ElementalBursts: [{
-    name: "Burst",
-    elementalDMG: "PyroDMG",
-    talentDMG: "ElementalBurstDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[3.0327, 3.2143, 3.3959, 3.6320, 3.8136, 3.9952, 4.2313, 4.4674, 4.7034, 4.9395, 5.1756, 5.4117, 5.6478, 5.8838, 6.1199]]
-  }, {
-    name: "Low HP Burst",
-    elementalDMG: "PyroDMG",
-    talentDMG: "ElementalBurstDMG",
-    scaleStat: ["ATK"],
-    scaleValue: [[3.7909, 4.0179, 4.2449, 4.5400, 4.7670, 4.9940, 5.2891, 5.5842, 5.8793, 6.1744, 6.4695, 6.7646, 7.0597, 7.3548, 7.6499]]
-  }],
-  Passives: [{
-    name: lang_1.Language.val("SanguineRouge"),
-    description: lang_1.Language.val("SanguineRougeDesc"),
-    effect: function (e) {
-      return [e.createModifier("PyroDMG", 0.33)];
-    }
-  }],
-  Constellations: [{
-    name: lang_1.Language.val("OminousRainfall"),
-    description: lang_1.Language.val("OminousRainfallDesc"),
-    effect: function (e) {
-      var modifier = e.createModifier("ElementalSkillDMG");
-      modifier.onEnable(function (e) {
-        var skill = e.getDamageInstance(lang_1.Language.val("BloodBlossom"));
-        if (!skill) return;
-        var n = skill.additiveCount;
-
-        if (n == 1) {
-          skill.addAdditive(0.1, "HP");
-        }
-      });
-      modifier.onDisable(function (e) {
-        var skill = e.getDamageInstance(lang_1.Language.val("BloodBlossom"));
-        if (!skill) return;
-        var n = skill.additiveCount;
-
-        if (n == 2) {
-          skill.removeAdditive(1);
-        }
-      });
-      return [modifier];
-    }
-  }]
-};
-
-/***/ }),
-
 /***/ "./built/damage/DamageInstance.js":
 /*!****************************************!*\
   !*** ./built/damage/DamageInstance.js ***!
@@ -346,6 +160,192 @@ var DamageInstance = function () {
 }();
 
 exports.DamageInstance = DamageInstance;
+
+/***/ }),
+
+/***/ "./built/datacharacters/characterList.js":
+/*!***********************************************!*\
+  !*** ./built/datacharacters/characterList.js ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.characterList = void 0;
+
+var charHuTao_1 = __webpack_require__(/*! ./list/charHuTao */ "./built/datacharacters/list/charHuTao.js");
+
+exports.characterList = {
+  "Hu Tao": charHuTao_1.charHuTao
+};
+
+/***/ }),
+
+/***/ "./built/datacharacters/list/charHuTao.js":
+/*!************************************************!*\
+  !*** ./built/datacharacters/list/charHuTao.js ***!
+  \************************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.charHuTao = void 0;
+
+var lang_1 = __webpack_require__(/*! ../../langs/lang */ "./built/langs/lang.js");
+
+exports.charHuTao = {
+  name: "Hu Tao",
+  ATKbase: [8, 21, 29, 43, 48, 55, 62, 69, 74, 81, 86, 94, 99, 106],
+  HPbase: [1211, 3141, 4179, 6253, 6990, 8042, 9026, 10089, 10826, 11899, 12637, 13721, 14459, 15552],
+  DEFbase: [68, 177, 235, 352, 394, 453, 508, 568, 610, 670, 712, 773, 815, 876],
+  scaleValue: [0, 0, 0, 0, 0.096, 0.096, 0.192, 0.192, 0.192, 0.192, 0.288, 0.288, 0.384, 0.384],
+  scaleStat: "CRITDMG",
+  NormalAttacks: [{
+    name: "1-Hit",
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "NormalAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.4689, 0.5008, 0.5328, 0.5754, 0.6074, 0.6447, 0.6926, 0.7406, 0.7885, 0.8365, 0.8844, 0.9324, 0.9804, 1.0283, 1.0763]]
+  }, {
+    name: "2-Hit",
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "NormalAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.4825, 0.5154, 0.5483, 0.5922, 0.6251, 0.6635, 0.7128, 0.7622, 0.8115, 0.8609, 0.9102, 0.9596, 1.0089, 1.0583, 1.1076]]
+  }, {
+    name: "3-Hit",
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "NormalAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.6105, 0.6521, 0.6938, 0.7493, 0.7909, 0.8394, 0.9019, 0.9643, 1.0268, 1.0892, 1.1516, 1.2141, 1.2765, 1.3389, 1.4014]]
+  }, {
+    name: "4-Hit",
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "NormalAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.6564, 0.7012, 0.7459, 0.8056, 0.8503, 0.9026, 0.9697, 1.0368, 1.1040, 1.1711, 1.2382, 1.3054, 1.3725, 1.4396, 1.5068]]
+  }, {
+    name: "5-Hit",
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "NormalAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.6847, 0.7314, 0.7781, 0.8404, 0.8870, 0.9415, 1.0115, 1.0816, 1.1516, 1.2216, 1.2917, 1.3617, 1.4317, 1.5018, 1.5718]]
+  }, {
+    name: "6-Hit",
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "NormalAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.8596, 0.9182, 0.9768, 1.0549, 1.1136, 1.1819, 1.2698, 1.3578, 1.4457, 1.5336, 1.6215, 1.7094, 1.7973, 1.8852, 1.9731]]
+  }, {
+    name: lang_1.Language.val("ChargedAttack"),
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "ChargedAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[1.3596, 1.4523, 1.5450, 1.6686, 1.7613, 1.8695, 2.0085, 2.1476, 2.2866, 2.4257, 2.5647, 2.7038, 2.8428, 2.9819, 3.1209]]
+  }, {
+    name: lang_1.Language.val("Plunge"),
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "PlungeAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.6542, 0.6988, 0.7434, 0.8029, 0.8475, 0.8995, 0.9664, 1.0333, 1.1002, 1.1671, 1.2340, 1.3010, 1.3679, 1.4348, 1.5017]]
+  }, {
+    name: lang_1.Language.val("LowPlunge"),
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "PlungeAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[1.3081, 1.3973, 1.4865, 1.6054, 1.6946, 1.7986, 1.9324, 2.0662, 2.2000, 2.3338, 2.4676, 2.6013, 2.7351, 2.8689, 3.0027]]
+  }, {
+    name: lang_1.Language.val("HighPlunge"),
+    elementalDMG: "PhysicalDMG",
+    talentDMG: "PlungeAttackDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[1.6339, 1.7453, 1.8567, 2.0052, 2.1166, 2.2466, 2.4137, 2.5808, 2.7479, 2.9150, 3.0821, 3.2492, 3.4163, 3.5834, 3.7505]]
+  }],
+  ElementalSkills: [{
+    name: lang_1.Language.val("BloodBlossom"),
+    elementalDMG: "PyroDMG",
+    talentDMG: "ElementalSkillDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[0.6400, 0.6880, 0.7360, 0.8000, 0.8480, 0.8960, 0.9600, 1.0240, 1.0880, 1.1520, 1.2160, 1.2800, 1.3600, 1.4400, 1.5200]]
+  }, {
+    name: lang_1.Language.val("ParamitaPapilio"),
+    description: lang_1.Language.val("ParamitaPapilioDesc"),
+    effect: function (e) {
+      var bonuses = [0.0384, 0.0407, 0.0430, 0.0460, 0.0483, 0.0506, 0.0536, 0.0566, 0.0596, 0.0626, 0.0656, 0.0685, 0.0715, 0.0745, 0.0775];
+      var bonus = bonuses[e.ElementalSkillLevel - 1];
+      var ATKincrease = e.createModifier("ATKflat");
+      ATKincrease.value = Math.min(bonus * e.HP, 4 * e.ATKbase);
+      e.createObserver("HPbase", "HPflat", "HPpercent", "ATKbase", "ElementalSkillLevel").onUpdate(function (e) {
+        bonus = bonuses[e.ElementalSkillLevel - 1];
+        ATKincrease.value = Math.min(bonus * e.HP, 4 * e.ATKbase);
+        ;
+      });
+      ATKincrease.onEnable(function (e) {
+        e.infuseDamageInstance("NormalAttackDMG", "PyroDMG");
+        e.infuseDamageInstance("ChargedAttackDMG", "PyroDMG");
+        e.infuseDamageInstance("PlungeAttackDMG", "PyroDMG");
+      });
+      ATKincrease.onDisable(function (e) {
+        e.infuseDamageInstance("NormalAttackDMG", null);
+        e.infuseDamageInstance("ChargedAttackDMG", null);
+        e.infuseDamageInstance("PlungeAttackDMG", null);
+      });
+      return [ATKincrease];
+    }
+  }],
+  ElementalBursts: [{
+    name: "Burst",
+    elementalDMG: "PyroDMG",
+    talentDMG: "ElementalBurstDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[3.0327, 3.2143, 3.3959, 3.6320, 3.8136, 3.9952, 4.2313, 4.4674, 4.7034, 4.9395, 5.1756, 5.4117, 5.6478, 5.8838, 6.1199]]
+  }, {
+    name: "Low HP Burst",
+    elementalDMG: "PyroDMG",
+    talentDMG: "ElementalBurstDMG",
+    scaleStat: ["ATK"],
+    scaleValue: [[3.7909, 4.0179, 4.2449, 4.5400, 4.7670, 4.9940, 5.2891, 5.5842, 5.8793, 6.1744, 6.4695, 6.7646, 7.0597, 7.3548, 7.6499]]
+  }],
+  Passives: [{
+    name: lang_1.Language.val("SanguineRouge"),
+    description: lang_1.Language.val("SanguineRougeDesc"),
+    effect: function (e) {
+      return [e.createModifier("PyroDMG", 0.33)];
+    }
+  }],
+  Constellations: [{
+    name: lang_1.Language.val("OminousRainfall"),
+    description: lang_1.Language.val("OminousRainfallDesc"),
+    effect: function (e) {
+      var modifier = e.createModifier("ElementalSkillDMG");
+      modifier.onEnable(function (e) {
+        var skill = e.getDamageInstance(lang_1.Language.val("BloodBlossom"));
+        if (!skill) return;
+        var n = skill.additiveCount;
+
+        if (n == 1) {
+          skill.addAdditive(0.1, "HP");
+        }
+      });
+      modifier.onDisable(function (e) {
+        var skill = e.getDamageInstance(lang_1.Language.val("BloodBlossom"));
+        if (!skill) return;
+        var n = skill.additiveCount;
+
+        if (n == 2) {
+          skill.removeAdditive(1);
+        }
+      });
+      return [modifier];
+    }
+  }]
+};
 
 /***/ }),
 
@@ -1244,7 +1244,7 @@ var Character_1 = __webpack_require__(/*! ./Character */ "./built/model/Characte
 
 var Effect_1 = __webpack_require__(/*! ./Effect */ "./built/model/Effect.js");
 
-var characterList_1 = __webpack_require__(/*! ../characters/characterList */ "./built/characters/characterList.js");
+var characterList_1 = __webpack_require__(/*! ../datacharacters/characterList */ "./built/datacharacters/characterList.js");
 
 var talentMaxLevel = 15;
 
