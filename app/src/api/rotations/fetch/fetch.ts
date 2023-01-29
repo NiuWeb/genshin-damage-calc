87716e4e6ad0caa7def4c6387f25de5a6e36cbee
NiuWeb <niuweb1channel@gmail.com>
2023-01-28 21:11:07 -0500

    Fix rotations fetch from github server: - parse index as string instead of json.

--- app/src/api/rotations/fetch/fetch.ts
+++ app/src/api/rotations/fetch/fetch.ts
@@ -5,12 +5,35 @@ const indexUrlProd: string = import.meta.env.VITE_ROTATION_PROD_URL_INDEX
  * @returns The contents of all fetched rotation files
  */
 export async function fetchRotations(mode: "dev" | "prod"): Promise<string[]> {
-    const req = await fetch(mode === "dev" ? indexUrlDev : indexUrlProd)
+    if (mode === "dev") {
+        return dev()
+    }
+    return prod()
+}
+
+async function dev() {
+    const req = await fetch(indexUrlDev)
     const paths: string[] = await req.json()
 
     const contents = await Promise.all(
         paths.map(async path => {
             const req = await fetch(path)
+            return await req.text()
+        })
+    )
+
+    return contents
+}
+
+
+async function prod() {
+    const req = await fetch(indexUrlProd + "/index.txt")
+    const string = await req.text()
+    const paths = string.split("\n")
+
+    const contents = await Promise.all(
+        paths.map(async path => {
+            const req = await fetch(indexUrlProd + "/content/" + path.trim())
             return await req.text()
         })
     )
