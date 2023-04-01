58c48b16dc1cc7d1b2e77e5fc7e84aa9a8a7ef63
NiuWeb <niuweb1channel@gmail.com>
2023-04-01 03:49:36 -0500

    Display multiple versions in changelog

--- app/src/api/latest/index.ts
+++ app/src/api/latest/index.ts
@@ -1,4 +1,4 @@
-const ApiUrl = import.meta.env.VITE_GITHUB_LATEST_URL
+const ApiUrl = import.meta.env.VITE_GITHUB_RELEASES_API_URL
 
 export interface GithubLatest {
     name: string
@@ -7,7 +7,7 @@ export interface GithubLatest {
     body: string
 }
 
-export async function FetchGithubLatest(): Promise<GithubLatest | undefined> {
+export async function FetchGithubLatest(): Promise<GithubLatest[] | undefined> {
     try {
         const req = await fetch(ApiUrl)
         const json = await req.json()
--- app/src/components/github/latest/index.tsx
+++ app/src/components/github/latest/index.tsx
@@ -7,30 +7,37 @@ import { useEffect, useState } from "react"
 import { Github } from "react-bootstrap-icons"
 import "./style.css"
 
+const releasesUrl = import.meta.env.VITE_GITHUB_RELEASES_URL
+
 export function GithubLatest() {
-  const [content, setContent] = useState<IContent | undefined>(undefined)
+  const [content, setContent] = useState<IContent[] | undefined>(undefined)
   useEffect(() => {
     FetchGithubLatest().then(content => {
       if (!content) { return }
-      setContent(content)
+      setContent(content.slice(0, 3))
     })
   }, [])
 
 
-  return !!content ? (
+  return content ? (
     <Modal show onClose={() => setContent(undefined)}>
       <ModalHeader>
-        {content.name} ({new Date(content.published_at).toUTCString()})
+        {GetString("LABEL.CHANGELOG")}
       </ModalHeader>
       <ModalBody>
-        <div className="md-document">
-          <Markdown linebreak>
-            {content.body}
-          </Markdown>
-        </div>
+        {content.map((content, i) => (
+          <div key={i} className="md-document">
+            <h2 className="text-lg my-1 font-bold">
+              {content.name} ({new Date(content.published_at).toUTCString()})
+            </h2>
+            <Markdown linebreak>
+              {content.body}
+            </Markdown>
+          </div>
+        ))}
       </ModalBody>
       <ModalFooter>
-        <a target="_blank" href={content.html_url}>
+        <a target="_blank" href={releasesUrl}>
           <div
             role="button"
             className="github inline-flex p-2 bg-black/80 hover:bg-black/90 active:bg-black text-white gap-1 items-center">
