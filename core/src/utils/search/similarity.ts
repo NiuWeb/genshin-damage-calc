ebee85be6f5dee9af3cab8066ce62c67019e2d3e
NiuWeb <niuweb1channel@gmail.com>
2023-06-18 14:25:37 -0500

    search first for exact substrings
    if using only similarity index, "emblem" will
    match "gambler" first than
    "emblemofseveredfate", so now it will
    search for exact substring matches first,
    and if found, the similarity index will be
    calculated as the length ratio:
    `item/query`

--- core/src/utils/search/similarity.ts
+++ core/src/utils/search/similarity.ts
@@ -9,15 +9,22 @@ export interface SimilarityProps {
     /** The number of results to return */
     count?: number
     /** The minimum similarity threshold */
-    threshold?: number
+    threshold?: number,
+    /** search first for exact substring matches? */
+    substring?: boolean
 }
 /**
  * Search the n most similar strings in a list of strings
  */
 export function searchSimilarStrings(props: SimilarityProps) {
-    const { list, query, count = 1, threshold = 0.5 } = props
+    const { list, query, count = 1, threshold = 0.5, substring = true } = props
     const queue = new PriorityQueue<string>(count)
     for (const item of list) {
+        if (substring && item.includes(query)) {
+            const lenDif = item.length / query.length
+            queue.Push(item, lenDif)
+            continue
+        }
         const similarity = stringSimilarity(query, item)
         if (similarity >= threshold) {
             queue.Push(item, similarity)
