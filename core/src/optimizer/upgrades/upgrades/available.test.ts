4f87860c38115b1e12f4e00c56218f5b4c0addae
NiuWeb <niuweb1channel@gmail.com>
2023-07-30 00:51:37 -0500

    add more tests for available upgrades

--- core/src/optimizer/upgrades/upgrades/available.test.ts
+++ core/src/optimizer/upgrades/upgrades/available.test.ts
@@ -41,6 +41,54 @@ describe("Available Upgrades are detected correcly", () => {
             }
         }
     })
+
+    test("character and talent levels with constellation #1", () => {
+        const runner = new Runner()
+        runner.Program.CompileString(`
+            character add xiangling
+            character level 80+
+
+            effect set xianglingC3
+            effect enable
+            effect set xianglingC5
+            effect enable
+
+            character talent 1 11 11
+        `)()
+        const char = runner.Scenario.GetChar()
+
+        // upgrades available are:
+        // - level 90
+        // - normal attack level 2
+        // - elemental skill level 12
+        // - elemental burst level 12
+
+        const upgrades = GetUpgrades(char)
+        expect(upgrades).toHaveLength(4)
+
+        // assert correct upgrades
+        const types = upgrades.map(u => u.type)
+        expect(types).toContain(Upgrade.CHARACTER_LEVEL)
+        expect(types).toContain(Upgrade.NORMAL_ATTACK_LEVEL)
+        expect(types).toContain(Upgrade.ELEMENTAL_SKILL_LEVEL)
+        expect(types).toContain(Upgrade.ELEMENTAL_BURST_LEVEL)
+
+        // assert correct values
+        for (const upgrade of upgrades) {
+            if (upgrade.type === Upgrade.CHARACTER_LEVEL) {
+                expect(upgrade.value).toBe(90)
+            }
+            if (upgrade.type === Upgrade.NORMAL_ATTACK_LEVEL) {
+                expect(upgrade.value).toBe(2)
+            }
+            if (upgrade.type === Upgrade.ELEMENTAL_SKILL_LEVEL) {
+                expect(upgrade.value).toBe(9)
+            }
+            if (upgrade.type === Upgrade.ELEMENTAL_BURST_LEVEL) {
+                expect(upgrade.value).toBe(9)
+            }
+        }
+    })
     
     test("character and talent levels with constellation", () => {
         const runner = new Runner()
