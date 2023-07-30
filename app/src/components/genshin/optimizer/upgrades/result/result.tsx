ab2cd89e9f6fc9326da63e4229ed0323226e2205
NiuWeb <niuweb1channel@gmail.com>
2023-07-30 18:21:51 -0500

    add export to image button for upgrades table

--- app/src/components/genshin/optimizer/upgrades/result/result.tsx
+++ app/src/components/genshin/optimizer/upgrades/result/result.tsx
@@ -1,10 +1,27 @@
+import { exportImage } from "@src/components/datatable/export/image"
 import { genshin } from "@src/genshin/core"
+import { GetString } from "@src/strings/strings"
+import { useRef } from "react"
 import { UpgradesChart } from "./chart/chart"
 import { UpgradesTable } from "./table/table"
 
 export function UpgradesResults({ results }: { results: genshin.optimizer.upgrades.Result[][] }) {
-  return <div className="upgrades-result">
+
+  const ref = useRef<HTMLDivElement>(null)
+  async function exportImg() {
+    if (!ref.current) {
+      return
+    }
+    await exportImage(ref.current)
+  }
+
+  return <div className="upgrades-result flex flex-col gap-1">
     <UpgradesChart results={results} />
-    <UpgradesTable results={results} />
+    <button onClick={exportImg} className="bg-gray-600 hover:bg-gray-700">
+      {GetString("ACTION.EXPORT.IMAGE")}
+    </button>
+    <div className="table-container bg-neutral-800" ref={ref}>
+      <UpgradesTable results={results} />
+    </div>
   </div>
 }
