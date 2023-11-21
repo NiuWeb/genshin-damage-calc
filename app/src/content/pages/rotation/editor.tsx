b9432e24eae7d9ac4a4d51eea0a3b3b0e168d8f5
NiuWeb <niuweb1channel@gmail.com>
2023-11-20 20:10:57 -0500

    add code screenshot  button

--- app/src/content/pages/rotation/editor.tsx
+++ app/src/content/pages/rotation/editor.tsx
@@ -1,3 +1,5 @@
+import { Code } from "@src/components/console/code"
+import { exportImage } from "@src/components/datatable/export/image"
 import { MonacoEditor } from "@src/components/monaco/monaco"
 import { useCalc } from "@src/genshin/context"
 import { genshin } from "@src/genshin/core"
@@ -5,10 +7,27 @@ import { Alert } from "@src/popup/alert"
 import { Loading } from "@src/popup/loading"
 import { GetString } from "@src/strings/strings"
 import { PrettyMs } from "@src/utils/pretty-ms"
-import { ArrowReturnRight } from "react-bootstrap-icons"
+import { useEffect } from "react"
+import { ArrowReturnRight, Camera } from "react-bootstrap-icons"
+import { createRoot } from "react-dom/client"
 import { useNavigate } from "react-router"
 
 const EDITOR_FNAME = "rotation_editor"
+
+const imageRoot = document.createElement("div")
+document.body.appendChild(imageRoot)
+imageRoot.id = "rotation-editor-image-root"
+imageRoot.setAttribute("style", "position: absolute; top: 0; left: 0; width: 100%")
+
+
+function Render({ children, onRender }: { children: React.ReactNode, onRender?: () => void }) {
+
+  useEffect(() => {
+    onRender?.()
+  }, [])
+
+  return <>{children}</>
+}
 
 export function PageRotationEditor() {
   const [calc, exec] = useCalc()
@@ -45,8 +64,27 @@ export function PageRotationEditor() {
     }
   }
 
+  async function photo() {
+    const code = calc.Editor.GetFile(EDITOR_FNAME)
+    imageRoot.innerHTML = ""
+
+    const root = createRoot(imageRoot)
+
+    await new Promise<void>((resolve) => {
+      root.render(<Render onRender={resolve}>
+        <Code pre>{code}</Code>
+      </Render>)
+    })
+
+    await new Promise<void>(resolve => setTimeout(resolve, 100))
+
+    await exportImage(imageRoot)
+    root.unmount()
+    imageRoot.innerHTML = ""
+  }
+
   return <div className="rotation-editor flex flex-col h-full">
-    <div className="rotation-editor-header">
+    <div className="rotation-editor-header flex justify-between">
       <button
         onClick={run}
         className="text-black px-2 py-1 bg-green-600 hover:bg-green-700 active:bg-green-800">
@@ -54,6 +92,12 @@ export function PageRotationEditor() {
           <ArrowReturnRight />
           {GetString("ACTION.RUN")}
         </div>
+      </button>
+
+      <button
+        onClick={photo}
+        className="text-black px-2 py-1 bg-blue-600 hover:bg-blue-700 active:bg-blue-800">
+        <Camera />
       </button>
     </div>
     <div className="grow">
